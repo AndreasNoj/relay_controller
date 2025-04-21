9210a5d9eca87b95237ba1c1714d091b5b60cfd8
Andreas Nøjgaard <Andreas@an-concepts.dk>
2025-04-21 15:10:38 +0200

    SenseAppBuilder param update

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,8 @@ void setup() {
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     ->set_hostname("Light-Inside-Relays")
-                    ->set_wifi_client("Obelix", "obelix2idefix")
-                    ->enable_ota("SomeOTAPassword")
-                    ->enable_uptime_sensor()
+                    ->enable_ota("LilleMyOTAPassword")
+  //                  ->enable_uptime_sensor()
                     ->get_app();
 
   // GPIO configuration:
